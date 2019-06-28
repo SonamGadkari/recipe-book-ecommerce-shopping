a9e793e9aa61d10d232daf082bbdf71067a044e3
Sonam Gadekari <S533490@nwmissouri.edu>
2019-06-28 16:36:57 -0500

    Created project & add bootstrap header & Recipes logic in angular

---
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  // tslint:disable-next-line:max-line-length
+  recipes: Recipe[] = [new Recipe('A test Recipe', 'This is good recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Authentic Recipe', 'This is good recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')];
+  constructor() { }
+  getrecipeName(onerecipe: Recipe ) {
+    return onerecipe.name;
+  }
+  getrecipeDescription(onerecipe: Recipe) {
+    return onerecipe.description;
+  }
+  getimagePath(onerecipe: Recipe ) {
+    return onerecipe.imagePath;
+  }
+  ngOnInit() {
+  }
+
+}
